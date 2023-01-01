eb862e4d59e2572160bf96c2f94d5579fdedb4b4
RioAdistya <rioputra191101@gmail.com>
2023-01-01 17:24:08 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+
+    container: {
+      center: true,
+      padding: '16px',
+    },
+
+    extend: {
+      colors: {
+        utama: '#14b8a6',
+        gelap: '#0f172a',
+        kedua: '#64748b',
+      },
+
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
